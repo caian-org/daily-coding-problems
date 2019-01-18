541662d5736420b78103c38508838a4837f884ae
Caian R. Ertl <caianrais@protonmail.com>
2019-01-18 02:26:37 -0200

    feat: add the NodeJS implementation for problem #2

---
+++ problems/2/node/main.js
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+
+
+const prodOfAllBrute = (arr) =>
+{
+    const a = [];
+
+    for(let i = 0; i < arr.length; i++)
+    {
+        let x = 1;
+        for(let j = 0; j < arr.length; j++)
+        {
+            if(arr[i] == arr[j])
+                continue;
+
+            x *= arr[j];
+        }
+
+        a.push(x);
+    }
+
+    return a;
+}
+
+
+const prodOfAll = (arr) =>
+{
+    const q = arr.length,
+        a = new Array(q),
+        b = new Array(q);
+
+    let p = 1;
+    for(let i = 0; i < q; i++)
+    {
+        a[i] = p;
+        p *= arr[i];
+    }
+
+    p = 1;
+    for(let j = (q - 1); j >= 0; j--)
+    {
+        b[j] = p;
+        p *= arr[j];
+    }
+
+    const r = [];
+    for(let k = 0; k < q; k++)
+        r.push(a[k] * b[k]);
+
+    return r;
+}
+
+
+const L = [1, 2, 3, 4, 5];
+
+
+console.log(prodOfAll(L));
