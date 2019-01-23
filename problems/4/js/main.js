fa3b7c660c1c73d28da58d60eba8fb1f3c45018e
Caian R. Ertl <caianrais@protonmail.com>
2019-01-23 00:50:18 -0200

    feat(4): add the JS implementation

---
+++ problems/4/js/main.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+
+const minInt = (arr) =>
+{
+    arr = arr
+        .map((x)    => { return x })
+        .filter((x) => { return x > 0 });
+
+    arr.sort();
+
+    for(let i = 0; i < arr.length; i++)
+    {
+        if(i == (arr.length - 1))
+            return (arr[i] + 1);
+
+        if(arr[i + 1] - arr[i] > 1)
+            return (arr[i] + 1);
+    }
+}
+
+
+const x = [3, 4, -1, 1];
+
+
+console.log(minInt(x));
